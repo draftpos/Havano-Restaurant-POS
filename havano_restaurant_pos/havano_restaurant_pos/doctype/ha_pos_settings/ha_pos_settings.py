7fc5d0beb90437412241d2ac147c681ea2c29f14
Makoni Ashley Tadiswa <126552692+makonia20@users.noreply.github.com>
2026-04-17 11:59:00 +0200

    Update ha pos settings py

--- havano_restaurant_pos/havano_restaurant_pos/doctype/ha_pos_settings/ha_pos_settings.py
+++ havano_restaurant_pos/havano_restaurant_pos/doctype/ha_pos_settings/ha_pos_settings.py
@@ -1,29 +1,30 @@
 # Copyright (c) 2025, Chipo and contributors
 # For license information, please see license.txt
-
 import frappe
 from frappe.model.document import Document
 from frappe import _
 
-
 class HAPOSSettings(Document):
-	def validate(self):
-		# Check if hotel app is installed when enabling room direct bookings
-		if self.enable_room_direct_bookings:
-			if "havano_hotel_management" not in frappe.get_installed_apps():
-				frappe.throw(
-					_("Havano Hotel Management app is not installed. Please install it first to enable Room Direct Bookings."),
-					title=_("Hotel App Required")
-				)
+    def validate(self):
+        # Check if hotel app is installed when enabling room direct bookings
+        if self.enable_room_direct_bookings:
+            if "havano_hotel_management" not in frappe.get_installed_apps():
+                frappe.throw(
+                    _("Havano Hotel Management app is not installed. Please install it first to enable Room Direct Bookings."),
+                    title=_("Hotel App Required")
+                )
 
+    def before_save(self):
+        for row in self.user_mapping:
+            # check that cost_center belongs to the selected company
+            if row.cost_center:
+                cost_center_doc = frappe.get_doc("Cost Center Details", row.cost_center)
+                if cost_center_doc.company_name != row.company:
+                    frappe.throw(
+                        _("Row {0}: Cost Center '{1}' does not belong to Company '{2}'")
+                        .format(row.idx, row.cost_center, row.company)
+                    )
 
-	def before_save(self):
-		for row in self.user_mapping:
-			# check that cost_center belongs to the selected company
-			if row.cost_center:
-				cost_center_doc = frappe.get_doc("Cost Center Details", row.cost_center)
-				if cost_center_doc.company_name != row.company:
-					frappe.throw(
-						_("Row {0}: Cost Center '{1}' does not belong to Company '{2}'")
-						.format(row.idx, row.cost_center, row.company)
-					)
+    @frappe.whitelist()
+    def get_fiscalisation_enabled():
+        return frappe.db.get_single_value("HA POS Settings", "enable_fiscalisation")
